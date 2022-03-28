7f9d8f603454517a6429b43e5ce6e07510ae6888
bakr <you@example.com>
2022-03-28 22:39:50 +0200

    deploy

---
+++ _next/static/chunks/ae51ba48-3a7c4c2bad3604b2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{155:function(t,n,e){e.d(n,{MB4:function(){return i}});var r=e(8357);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M96 256h320M96 176h320M96 336h320"}}]})(t)}}}]);
